5d9199d2e368caa84e5bdd87043d0af909619a23
Michael Richardson <mcr@sandelman.ca>
2011-02-01 10:44:47 -0500

    renamed helpers

--- app/helpers/franglish_helper.rb
+++ app/helpers/franglish_helper.rb
@@ -1,13 +1,13 @@
-module Clf2Helper
+module FranglishHelper
   # Wraps the render_menu in the Redmine core but will apply CLF
   # specific CSS to the items including the outer menu and inner menu
-  def render_clf_menu(menu, project=nil, options = {})
+  def render_franglish_menu(menu, project=nil, options = {})
     # Default options
     options = {
       :ul_class => 'nav',
       :li_class => 'menucontent',
       :menulink_class => 'menulink',
-      :title => :clf2_text_top_menu
+      :title => :franglish_text_top_menu
     }.merge(options)
 
     # Menu links
@@ -28,16 +28,16 @@ module Clf2Helper
                                links.join("\n"),
                                :class => options[:ul_class])
 
-      return outer_clf_menu(inner_menu, options[:title], options)
+      return outer_franglish_menu(inner_menu, options[:title], options)
     end
   end
 
-  # Renders the outer CLF menu for inner_html and a title.
-  def outer_clf_menu(inner_html, title, options = {})
+  # Renders the outer FRANGLISH menu for inner_html and a title.
+  def outer_franglish_menu(inner_html, title, options = {})
     # Default options
     options = {
       :ul_class => 'nav',
-      :title => :clf2_text_top_menu
+      :title => :franglish_text_top_menu
     }.merge(options)
 
     content_tag(:ul,
@@ -49,18 +49,18 @@ module Clf2Helper
                 :class => options[:ul_class])
   end
 
-  # Wraps the Redmine core's render_main_main but using the CLF
-  # helpers to generate the correct CLF structure and classes
-  def render_clf_main_menu(project, options = {})
+  # Wraps the Redmine core's render_main_main but using the FRANGLISH
+  # helpers to generate the correct FRANGLISH structure and classes
+  def render_franglish_main_menu(project, options = {})
     # Default options
     options = {
       :ul_class => 'nav',
       :li_class => 'menucontent',
       :menulink_class => 'menulink',
-      :title => :clf2_text_main_menu
+      :title => :franglish_text_main_menu
     }.merge(options)
 
-    render_clf_menu((project && !project.new_record?) ? :project_menu : :application_menu, project, options)
+    render_franglish_menu((project && !project.new_record?) ? :project_menu : :application_menu, project, options)
   end
 
   # Truncate text based on word boundries
